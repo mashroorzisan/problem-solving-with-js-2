83cfb251b98789afa48c0eecee4200230b6eb573
mashroorzisan <mashroorzisan@gmail.com>
2022-08-04 00:32:42 +0600

    problem 1

--- prb1.js
+++ prb1.js
@@ -1,5 +1,14 @@
 // you are given a array of fruit basket
-// remove banana and replace mango
+// remove banana and replace it by mango
 // remove orange and add watermelon
 var fruits = ['Apple', 'Banana', 'Orange'];
-console.log(fruits[0]);
+// var indexBan = fruits.indexOf('Banana');
+// fruits.splice(indexBan, 1);
+// console.log(fruits);
+// fruits.insert(indexBan, 'Mango');
+// console.log(fruits);
+fruits[1] = 'Mango'
+console.log(fruits);
+fruits.pop();
+fruits.push('Watermelon')
+console.log(fruits);
