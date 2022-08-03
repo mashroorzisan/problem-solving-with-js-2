07f64df00c37d291da71de3ab762fb2422c7c4c7
mashroorzisan <mashroorzisan@gmail.com>
2022-08-04 01:13:42 +0600

    problem 3

---
+++ prb3.js
@@ -0,0 +1,13 @@
+// largest number problem
+var abab = 13;
+var rubab = 2;
+var kabab = 45;
+if (abab > rubab && abab > kabab) {
+    console.log("abab is the biggest");
+}
+else if (rubab > abab && rubab > kabab) {
+    console.log("rubab is the biggest");
+}
+else {
+    console.log("kabab is the biggest");
+}
