f2927f49460cd6c4cf1bc8cc4e43f7d93ec099b7
mashroorzisan <mashroorzisan@gmail.com>
2022-08-04 00:14:04 +0600

    problem 1

---
+++ prb1.js
@@ -0,0 +1,5 @@
+// you are given a array of fruit basket
+// remove banana and replace mango
+// remove orange and add watermelon
+var fruits = ['Apple', 'Banana', 'Orange'];
+console.log(fruits[0]);
