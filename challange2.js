31a94c8bffc29e9c64aa7a6fa5c792f630b87b97
mashroorzisan <mashroorzisan@gmail.com>
2022-08-04 01:26:44 +0600

    challange 1

---
+++ challange2.js
@@ -0,0 +1,16 @@
+var color;
+var myPos;
+myPos = 0;
+color = ['red', 'green', 'yellow'];
+if (myPos == 0) {
+    console.log(color[myPos]);
+    console.log("Don't Move")
+}
+else if (myPos == 1) {
+    console.log(color[myPos]);
+    console.log("Move it");
+}
+else {
+    console.log(color[myPos]);
+    console.log("Your wish");
+}
